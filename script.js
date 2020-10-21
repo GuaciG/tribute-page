178e478fc00209c171a248696dc0cc9f1d989be2
Guaci Gutierrez <guacig@hotmail.com>
2020-10-21 12:55:29 +0100

    fixed path to stylesheet file

--- script.js
+++
@@ -1,8 +0,0 @@
-// javaScript for navigation bar in small devices
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}    
